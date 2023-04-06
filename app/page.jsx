e4e819940e011d8174f952df0bd584f174a7c14d
Sikandar <sikandar.ramzan@codeninjaconsulting.com>
2023-04-07 00:08:52 +0500

    fix typo in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,7 @@ const HomePage = () => {
 
   return (
     <>
-      <h1>Welcome To Traversy Meida</h1>
+      <h1>Welcome To Traversy Media</h1>
       <CourseSearch getSearchResults={(results) => setCourses(results)} />
       <Courses courses={courses} />
     </>
